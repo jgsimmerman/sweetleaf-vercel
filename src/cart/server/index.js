e8b15c1260218dbe9e40f0c2a7d5751ec5592c88
Jacob <jgsimmerman@gmail.com>
2020-01-19 17:35:04 -0500

    Fix some shipping methods

--- src/cart/server/index.js
+++ src/cart/server/index.js
@@ -1,6 +1,6 @@
 import submitStripeInfo from './submit-stripe-info'
 import submitStripeOrder from './submit-stripe-order'
-import updateShipping from './updateShipping'
+import updateShipping from './update-shipping'
 //import sendSparkpostConfirmation from './send-sparkpost-confirmation'
 
 export {
