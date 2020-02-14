4d24fd5e3dd3ceef0dd3067fa86b01a432781c01
Jacob <jgsimmerman@gmail.com>
2020-02-14 11:30:36 -0500

    npm install invariant

--- src/pages/coupons.jsx
+++ src/pages/coupons.jsx
@@ -1,22 +1,22 @@
-import React from "react"
-import { Link } from "gatsby"
-import { Layout, Container } from 'layouts';
-//import SEO from "../components/SEO"
-import Coupons from "../components/Products/Coupons"
-const coupons = () => (
-  <Layout>
-    <div >
-    <p>
-      <br />
-      <br />
-      <br />
-      <hr />
-    </p>
-    </div>
-    <h1>These are the coupons fetched by the GraphQL query.</h1>
-    <Coupons />
-    <hr />
+// import React from "react"
+// import { Link } from "gatsby"
+// import { Layout, Container } from 'layouts';
+// //import SEO from "../components/SEO"
+// import Coupons from "../components/Products/Coupons"
+// const coupons = () => (
+//   <Layout>
+//     <div >
+//     <p>
+//       <br />
+//       <br />
+//       <br />
+//       <hr />
+//     </p>
+//     </div>
+//     <h1>These are the coupons fetched by the GraphQL query.</h1>
+//     <Coupons />
+//     <hr />
     
-  </Layout>
-)
-export default coupons;
+//   </Layout>
+// )
+// export default coupons;
