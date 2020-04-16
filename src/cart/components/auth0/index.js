fb7cc6599a3b1df9116a81ff1b74c4b49d118a30
Jacob <jgsimmerman@gmail.com>
2020-04-16 01:47:53 -0400

    edit out auth

--- src/cart/components/auth0/index.js
+++ src/cart/components/auth0/index.js
@@ -111,6 +111,111 @@ export default class Login extends React.Component {
 	}
 }
 
+export class LoginPopup extends React.Component {
+	constructor(props) {
+		super(props)
+		this.logout = this.logout.bind(this)
+		this.show = this.show.bind(this)
+	}
+
+	componentDidMount() {
+		if (settingsState.state.auth0ClientID && settingsState.state.auth0Domain) {
+			this.lock = new Auth0Lock(
+				settingsState.state.auth0ClientID,
+				settingsState.state.auth0Domain,
+				{
+					autoclose: true,
+					closeable: true,
+					theme: {
+						logo: ``,
+						...settingsState.state.auth0Theme,
+					},
+					languageDictionary: {
+						title: `Log In`,
+						emailPlaceholder: `something@youremail.com`,
+					},
+					...settingsState.state.auth0Options,
+				}
+			)
+
+			this.lock.on(`authenticated`, (authResult) => {
+				// Use the token in authResult to getUserInfo() and save it to localStorage
+				this.lock.getUserInfo(authResult.accessToken, (error, profile) => {
+					if (error) {
+						return
+					}
+					localStorage.setItem(`accessToken`, authResult.accessToken) // eslint-disable-line no-undef
+					localStorage.setItem(`profile`, JSON.stringify(profile)) // eslint-disable-line no-undef
+					customerState.setState({ customer: profile })
+				})
+			})
+
+			this.lock.checkSession({}, (err, authResult) => {
+				if (err) {
+					return
+				}
+				this.lock.getUserInfo(authResult.accessToken, (error, profile) => {
+					if (error) {
+						return
+					}
+					localStorage.setItem(`accessToken`, authResult.accessToken) // eslint-disable-line no-undef
+					localStorage.setItem(`profile`, JSON.stringify(profile)) // eslint-disable-line no-undef
+					customerState.setState({ customer: profile })
+				})
+			})
+		}
+	}
+
+	show() {
+		var options = {
+			additionalSignUpFields: [{
+				name: `address`,
+				placeholder: `enter your address`,
+				// The following properties are optional
+				icon: `https://example.com/assests/address_icon.png`,
+				prefill: `street 123`,
+				validator: function(address) {
+					return {
+						valid: address.length >= 10,
+						hint: `Must have 10 or more chars`, // optional
+					}
+				},
+			},
+			{
+				name: `full_name`,
+				placeholder: `Enter your full name`,
+			}],
+		}
+		this.lock.show(options)
+	}
+
+	logout() {
+		this.lock.logout({
+			returnTo: settingsState.state.auth0Logout,
+		})
+		localStorage.removeItem(`profile`) // eslint-disable-line no-undef
+		customerState.reset()
+	}
+
+	render() {
+		return (
+			<Subscribe to={customerState}>
+				{({ customer }) => (
+					<div>
+						{this.lock && !customer && this.lock.show()}
+						{this.lock && customer && 
+							<div>
+								<div>Welcome <span onClick={this.show}>{customer.nickname}</span>!</div>
+								<button className="zygoteBtn zygoteBtnSmall zygotePrimaryBtn" onClick={() => this.logout()}>Log out</button>
+							</div>
+						}
+					</div>
+				)}
+			</Subscribe>
+		)
+	}
+}
+
 /*
 //////////////////////////////////
 					HOOKS
