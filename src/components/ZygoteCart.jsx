50ee64220f1e3894158795814f65d5ce1a909859
Jacob <jgsimmerman@gmail.com>
2020-04-20 23:25:39 -0400

    Edit out auth0

--- src/components/ZygoteCart.jsx
+++ src/components/ZygoteCart.jsx
@@ -56,26 +56,26 @@ const ZygoteCart = props => {
 
       cartHeader={<div>Sweet Leaf</div>}
 
-      auth0ClientID='2VYDe7FojdVooeMFbcWsf9rXEI0F8clm'
-      //auth0Logout='https://localhost:8000/'
-      auth0Logout='https://sweetleaf-gc.netlify.app/'
-      auth0Domain='dev-cbk6z20i.auth0.com'
-      auth0Theme={{
-        primaryColor: '#00cfff',
-      }}
-      auth0Options={{
-        avatar: null,
-        rememberLastLogin: false,
-        hashCleanup: true,
+      // auth0ClientID='2VYDe7FojdVooeMFbcWsf9rXEI0F8clm'
+      // //auth0Logout='https://localhost:8000/'
+      // auth0Logout='https://sweetleaf-gc.netlify.app/'
+      // auth0Domain='dev-cbk6z20i.auth0.com'
+      // auth0Theme={{
+      //   primaryColor: '#00cfff',
+      // }}
+      // auth0Options={{
+      //   avatar: null,
+      //   rememberLastLogin: false,
+      //   hashCleanup: true,
         
-        auth: {
-          // Solution from this link: https://github.com/auth0/lock/issues/1294
-           responseType: 'token',
-           redirect: false,
-           //redirectUrl: 'https://localhost:8000/'
-          redirectUrl: 'https://sweetleaf-gc.netlify.app/'
-        },
-      }}
+      //   auth: {
+      //     // Solution from this link: https://github.com/auth0/lock/issues/1294
+      //      responseType: 'token',
+      //      redirect: false,
+      //      //redirectUrl: 'https://localhost:8000/'
+      //     redirectUrl: 'https://sweetleaf-gc.netlify.app/'
+      //   },
+      // }}
       
 
       // totalModifications={[
