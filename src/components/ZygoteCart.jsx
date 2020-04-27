30335a568fad562561ab230e1b984a2c287f6e8a
Jacob <jgsimmerman@gmail.com>
2020-04-27 02:26:03 -0400

    rebuild

--- src/components/ZygoteCart.jsx
+++ src/components/ZygoteCart.jsx
@@ -63,6 +63,7 @@ const ZygoteCart = props => {
       // auth0Theme={{
       //   primaryColor: '#00cfff',
       // }}
+      
       // auth0Options={{
       //   avatar: null,
       //   rememberLastLogin: false,
