ef4888704c0ebfc19039f8a6ca83a494e5a71498
Jacob <jgsimmerman@gmail.com>
2020-05-02 18:56:15 -0400

    SEO

--- config/site.js
+++ config/site.js
@@ -8,7 +8,7 @@ module.exports = {
   siteUrl: 'https://sweetleafsucculents.com', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
   logo: 'static/logo/logo.png', // Used for SEO
-  banner: 'static/logo/banner.png',
+  banner: 'static/logo/logo.png',
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
   shortName: 'SweetLeaf', // shortname for manifest. MUST be shorter than 12 characters
--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -59,7 +59,7 @@ const SEO = ({ title, desc, banner, pathname, article }) => (
             brand: 'Sweet Leaf Succulents and Ornamental Plants',
             offers: {
               '@type': 'Offer',
-              price: price || '',
+              price: seo.price || '',
             },
             description: seo.description,
             //datePublished: buildTime,
