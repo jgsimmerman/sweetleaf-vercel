83ff03310dbb04542c4a8a81216ad09625da4a7b
Jacob <jgsimmerman@gmail.com>
2020-05-01 20:57:11 -0400

    Updated Ice Plant page

--- content/posts/16-soft/icePlant/index.md
+++ content/posts/16-soft/icePlant/index.md
@@ -35,3 +35,6 @@ available: 1
 story: "Corpuscularia lehmannii (Ice Plant)"
 
 ---
+Native to South Africa, Corpuscularia lehmannii (Ice Plant) has a deceptive nickname, as it is not cold hardy. If you live in a zone that gets colder than 30F, you should plant this the Ice Plant in a container that can be moved indoors. The Ice Plant does well in either full or partial sun; and when it is indoors, it's best to keep it near a sunny window. 
+
+In it's native South African habitat, the Ice Plant is largely water-deprived. Likewise, you should water you Ice Plant sparingly, when the soil has entirely dried out. When you do water, try to make sure the soil is completely soaked. This will allow your plant to soak up enough water to carry it through until the next "downpour," as it would in it's native habitat. 
--- src/cart/server/update-tax.js
+++ src/cart/server/update-tax.js
@@ -1,3 +1,62 @@
+// /// SEEMS TO WORK
+// const axios = require('axios');
+
+// let postalcode = 29069;
+// let rate1 = 0;
+
+// let credentials = 'MjAwMDE2NzI2Mjo5OUYxNzM2OEQzMEUzMTg1'
+
+// async function taxRate() { 
+        
+//            let rate = await axios({
+//                 url:`https://rest.avatax.com/api/v2/taxrates/bypostalcode?country=US&postalCode=${postalcode}`,
+//                 method: 'get',
+//                 headers: {'Authorization': 'Basic MjAwMDE2NzI2Mjo5OUYxNzM2OEQzMEUzMTg1'}
+//             })
+//             .then(  function (response) {
+//               // handle success
+//               console.log(`SUCCESS: `, response.data.totalRate);
+//               totalRate = response.data.totalRate
+//               return totalRate
+//             })
+            
+//            return rate
+// }
+
+
+// async function calc() {
+//   let totalRate =  await taxRate(postalcode)
+//   //console.log('rate ', rate)
+
+//   let obj = {'totalRate': totalRate, 'test': 'test'}
+//   let objStr = JSON.stringify(obj)
+//   //return obj
+//   return objStr
+// }
+
+// async function getTax() {
+//   let totalRate = await taxRate(postalcode)
+
+//   console.log('getTax', totalRate)
+//   return totalRate
+// }
+
+// //log(postalcode);
+
+// async function obj() {
+//   let print = await calc(postalcode)
+
+//   let printObj = JSON.parse(print)
+//   console.log('print', printObj)
+//   console.log('totalRate', printObj.totalRate)
+// }
+
+// obj()
+// /// ^ SEEMS TO WORK ^
+/////////////////////////////////////////////////////////////////////////////////
+
+
+
 // import { create } from 'apisauce';
 
 // let credentials = 'MjAwMDE2NzI2Mjo5OUYxNzM2OEQzMEUzMTg1'
@@ -10,7 +69,7 @@
 //     },
 //   });
 
-//   const getTaxRate = (postalCode) => api.get(`bypostalcode?country=US&postalCode=${postalcode}`);
+//   const getTaxRate = (postalcode) => api.get(`bypostalcode?country=US&postalCode=${postalcode}`);
 
 //   return {
 //     getTaxRate,
