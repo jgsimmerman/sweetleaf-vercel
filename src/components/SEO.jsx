418977b028a74eda3c00e2f73c4e42d12335a4d2
Jacob <jgsimmerman@gmail.com>
2020-05-02 10:56:05 -0400

    SEO component

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -3,7 +3,7 @@ import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 
-const SEO = ({ title, desc, banner, pathname, product }) => (
+const SEO = ({ title, desc, banner, pathname, product, article }) => (
   <StaticQuery
     query={query}
     render={({
