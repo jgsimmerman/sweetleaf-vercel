63923e896e3d74f960200b164887fe971b203021
Jacob <jgsimmerman@gmail.com>
2020-05-03 15:16:04 -0400

    image: seo.image

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -56,7 +56,7 @@ const SEO = ({ title, description, banner, pathname, price, sku, article }) => (
             //headline: title,
             image: {
               '@type': 'ImageObject',
-              url: logo || seo.image,
+              url: seo.image,
             },
             brand: 'Sweet Leaf Succulents and Ornamental Plants',
             offers: {
