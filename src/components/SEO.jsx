6baa12b71f023fe429c59e07fd66f5a92918de5b
Jacob <jgsimmerman@gmail.com>
2020-05-02 23:51:14 -0400

    `$${price}`

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -59,7 +59,7 @@ const SEO = ({ title, desc, banner, pathname, price, article }) => (
             brand: 'Sweet Leaf Succulents and Ornamental Plants',
             offers: {
               '@type': 'Offer',
-              'price': `$${seo.price}`,
+              'price': `$${price}`,
             },
             description: seo.description,
             //datePublished: buildTime,
