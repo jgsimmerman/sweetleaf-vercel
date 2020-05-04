ca483c9499992039422fd73c4c9e03b8c30a1c87
Jacob <jgsimmerman@gmail.com>
2020-05-04 02:56:30 -0400

    About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,7 +10,8 @@ const About = center => (
     <Header title="About Us"></Header>
     <Container center={center}>
       <h3>Welcome to Sweet Leaf Succulents!</h3>
-      <p>Based in Lamar, South Carolina; we specialize in succulents and ornamental plants.</p>
+      <p>Based in Lamar, South Carolina; we specialize in succulents and small ornamental plants that are ideal for fairy gardens, terrariums, or your workspace.</p>
+      <p>We take great care to ensure that your plant is packaged safe and securely so that it will show up in pristine condition. Each plant is hand inspected before shipping. We want you to love your plants as much as we do!</p>
       <p>Send us an email at <a href="mailto:sweetleafsucculents@gmail.com">sweetleafsucculents@gmail.com</a></p> 
     </Container>
   </Layout>
