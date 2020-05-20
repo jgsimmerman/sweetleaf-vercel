1b0f65725309720d52f860a0a405b7a1f44edc73
Jacob <jgsimmerman@gmail.com>
2020-05-20 11:32:47 -0400

    info-stripe function

--- api/cart/info-stripe.js
+++ api/cart/info-stripe.js
@@ -2,17 +2,32 @@ import dotenv from 'dotenv'
 import { submitStripeInfo } from '../../src/cart/server'
 dotenv.config({ silent: true })
 
-export async function handler({ body }) {
+// export async function handler({ body }) {
 
-	const res = await submitStripeInfo({
-		stripeApiSecret: process.env.STRIPE_API_SECRET,
+// 	const res = await submitStripeInfo({
+// 		stripeApiSecret: process.env.STRIPE_API_SECRET,
+// 		body,
+// 		verbose: true,
+// 	})
+
+// 	// Response
+// 	return {
+// 		statusCode: 200,
+// 		body: JSON.stringify(res),
+// 	}
+// }
+
+module.exports = (req, res) => {
+  let body = req.body,
+
+  const response = await submitStripeInfo({
+    stripeApiSecret: process.env.STRIPE_API_SECRET,
 		body,
 		verbose: true,
-	})
+  })
 
-	// Response
-	return {
+  return {
 		statusCode: 200,
-		body: JSON.stringify(res),
+		body: JSON.stringify(response),
 	}
 }
